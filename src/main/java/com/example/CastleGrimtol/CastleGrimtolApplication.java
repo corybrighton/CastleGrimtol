d5e364767f217bab9cbfb3ba820cd0e7ad4ebbde
Cory <cory.brighton@allata.com>
2020-05-21 16:31:07 -0600

    first commit

---
+++ src/main/java/com/example/CastleGrimtol/CastleGrimtolApplication.java
@@ -0,0 +1,13 @@
+package com.example.CastleGrimtol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastleGrimtolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastleGrimtolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CastleGrimtol/CastleGrimtolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CastleGrimtol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastleGrimtolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
